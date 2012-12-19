1e8d349f60e84bc2cb99b7bd7d6489f5bd697e0c
vincente <eyu8874521@163.com>
2012-12-19 16:12:41 +0800

    微信UI界面

---
+++ src/com/example/weichat/UI/FirstPageA.java
@@ -0,0 +1,16 @@
+package com.example.weichat.UI;
+
+import com.example.weichat.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FirstPageA extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.firstpage);
+	}
+
+}
